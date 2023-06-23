553efa59450fc198e17a2ad731ed9f04847f865e
ThomasBarker220 <james.barker@duke.edu>
2023-06-23 11:58:19 -0400

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,15 @@
+const { john, peter } = require("./4-names");
+
+const sayHi = require("./5-utils");
+
+const data = require("./6-alternative-flavor");
+
+require("./7-mind-grenade");
+
+// console.log(data);
+
+// console.log(names);
+
+sayHi("susan");
+sayHi(john);
+sayHi(peter);
